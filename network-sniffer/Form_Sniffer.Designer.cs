42d22b3d49e35dd1dd6d1d374c79aad40c62d84d
Niek <ik@niekvangogh.nl>
2018-10-24 09:58:12 +0200

    init commit

---
+++ network-sniffer/Form_Sniffer.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace network_sniffer
+{
+    partial class Form_Sniffer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textbox_messages = new System.Windows.Forms.TextBox();
+            this.button_start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textbox_messages
+            // 
+            this.textbox_messages.Location = new System.Drawing.Point(13, 13);
+            this.textbox_messages.Multiline = true;
+            this.textbox_messages.Name = "textbox_messages";
+            this.textbox_messages.Size = new System.Drawing.Size(775, 391);
+            this.textbox_messages.TabIndex = 0;
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(13, 415);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(75, 23);
+            this.button_start.TabIndex = 1;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // form_sniffer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.textbox_messages);
+            this.Name = "form_sniffer";
+            this.Text = "Sniffer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textbox_messages;
+        private System.Windows.Forms.Button button_start;
+    }
+}
+
---
+++ network-sniffer/form_sniffer.cs
@@ -0,0 +1,101 @@
+﻿using PcapDotNet.Core;
+using PcapDotNet.Packets;
+using PcapDotNet.Packets.IpV4;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace network_sniffer
+{
+    public partial class Form_Sniffer : Form
+    {
+        public Form_Sniffer()
+        {
+            InitializeComponent();
+        }
+
+        private void InitSniffer()
+        {
+            var packetDevice = this.GetNetworkDevice();
+            var communicator = packetDevice.Open(65536, PacketDeviceOpenAttributes.Promiscuous, 1000);
+
+            new Thread(() => this.Listen(communicator)).Start();
+            button_start.Enabled = false;
+        }
+
+        public void Listen(PacketCommunicator communicator)
+        {
+            do
+            {
+                var result = communicator.ReceivePacket(out var packet);
+                if (result == PacketCommunicatorReceiveResult.Ok)
+                {
+                    Datagram datagram = null;
+
+                    IpV4Datagram ip4 = packet.Ethernet.IpV4;
+                    if (ip4.Protocol == IpV4Protocol.Udp)
+                    {
+                        var udp = ip4.Udp;
+                        datagram = udp.Payload;
+                    }
+
+                    if (ip4.Protocol == IpV4Protocol.Tcp)
+                    {
+                        var tcp = ip4.Tcp;
+                        datagram = tcp.Payload;
+                    }
+
+                    string payload = this.ReadPayload(datagram);
+                    this.AddToLog(payload);
+                }
+            } while (true);
+        }
+
+        private void AddToLog(string payload)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AddToLog), new object[] {payload});
+                return;
+            }
+
+            this.textbox_messages.AppendText(payload + "\n");
+        }
+
+        private LivePacketDevice GetNetworkDevice()
+        {
+            IList<LivePacketDevice> devices = LivePacketDevice.AllLocalMachine;
+
+            if (devices.Count == 0)
+            {
+                MessageBox.Show("No interfaces found");
+            }
+
+            return devices[devices.Count - 2]; //gets the active connector thing
+        }
+
+        private string ReadPayload(Datagram datagram)
+        {
+            if (null != datagram)
+            {
+                int payloadLength = datagram.Length;
+                using (var ms = datagram.ToMemoryStream())
+                {
+                    byte[] rx_payload = new byte[payloadLength];
+                    ms.Read(rx_payload, 0, payloadLength);
+                    ASCIIEncoding encoder = new ASCIIEncoding();
+                    return encoder.GetString(rx_payload);
+                }
+            }
+
+            return null;
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            InitSniffer();
+        }
+    }
+}
